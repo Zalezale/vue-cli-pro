74a60822cae2ffd9c85238ee0fc9dbc3163ec9f1
Zalezale <506206437@qq.com>
2018-05-30 17:35:16 +0800

    实现懒加载组件

--- src/router/index.js
+++ src/router/index.js
@@ -3,11 +3,33 @@ import Router from 'vue-router'
 import login from '@/components/pages/login'
 import menu from '@/components/pages/menu'
 import usercenter from '@/components/pages/usercenter'
-// 功能页面
-import xsasnrp from "@/components/functions/xsasnrp"
-import xsplanrp from "@/components/functions/xsplanrp"
-import xsinvoicerp from "@/components/functions/xsinvoicerp"
-
+const xsasnrp = () => ({
+  // 需要加载的组件 (应该是一个 `Promise` 对象)
+  component: import(/* webpackChunkName: "xsasnrp" */'@/components/functions/xsasnrp'),
+  // 异步组件加载时使用的组件
+  loading: null,
+  // 加载失败时使用的组件
+  error: null,
+  // 展示加载时组件的延时时间。默认值是 200 (毫秒)
+  delay: 20000,
+  // 如果提供了超时时间且组件加载也超时了，
+  // 则使用加载失败时使用的组件。默认值是：`Infinity`
+  timeout: 30000
+})
+const xsplanrp = () => ({
+  component: import(/* webpackChunkName: "xsplanrp" */'@/components/functions/xsplanrp'),
+  loading: null,
+  error: null,
+  delay: 20000,
+  timeout: 30000
+})
+const xsinvoicerp = () => ({
+  component: import(/* webpackChunkName: "xsinvoicerp" */'@/components/functions/xsinvoicerp'),
+  loading: null,
+  error: null,
+  delay: 20000,
+  timeout: 30000
+})
 Vue.use(Router)
 
 export default new Router({
